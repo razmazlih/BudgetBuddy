a5ce506b1453e531a7ae68a0b642c449382bda73
Raz <retraz123@gmail.com>
2024-06-19 17:19:21 +0300

    Add documentation to functions

--- logic/data_management.py
+++ logic/data_management.py
@@ -13,14 +13,40 @@ expenses_file_location = os.path.join(
 )
 
 
-def get_data(file_location):
-    with open(file_location, "r") as budget_data:
-        json_data = json.load(budget_data)
+def get_data(file_path):
+    """
+    טוען נתונים מקובץ JSON נתון.
 
-    return json_data
+    פרמטרים:
+        file_path (str): הנתיב לקובץ ה-JSON.
+
+    מחזיר:
+        list: רשימת נתונים מהקובץ.
+
+    זורק:
+        Exception: אם יש שגיאה בטעינת הקובץ.
+    """
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
 
 def add_expense(date, category, amount, description):
+    """
+    מוסיף הוצאה חדשה לקובץ ההוצאות JSON.
+
+    פרמטרים:
+        date (str): התאריך של ההוצאה.
+        category (str): הקטגוריה של ההוצאה.
+        amount (float): סכום ההוצאה.
+        description (str): תיאור ההוצאה.
+
+    תיאור:
+        הפונקציה יוצרת מילון של ההוצאה החדשה עם התאריך, קטגוריה, סכום ותיאור.
+        היא טוענת את כל ההוצאות הקיימות מקובץ ה-JSON. אם יש שגיאה בטעינת הקובץ,
+        היא יוצרת רשימה ריקה. לאחר מכן, הפונקציה מוסיפה את ההוצאה החדשה לרשימה
+        ושומרת את הרשימה המעודכנת בחזרה לקובץ ה-JSON.
+
+    """
     expense_to_add = {
         "date": date,
         "category": category,
@@ -36,4 +62,4 @@ def add_expense(date, category, amount, description):
     all_expenses.append(expense_to_add)
 
     with open(expenses_file_location, "w") as expenses_file:
-        json.dump(all_expenses, expenses_file)
+        json.dump(all_expenses, expenses_file, indent=4)
