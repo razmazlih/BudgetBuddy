5dcb034a501959907a9ca1a7250009c297020a5f
Raz <retraz123@gmail.com>
2024-06-26 21:17:07 +0300

    put all CRUD code in one file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from logic.data_management import add_record, add_expense, add_income, search_expense, search_income, update_budget, delete_expense, delete_income
+from logic.data_management import (
+    add_record,
+    add_expense,
+    add_income,
+    search_expense,
+    search_income,
+    update_budget,
+    delete_expense,
+    delete_income,
+)
